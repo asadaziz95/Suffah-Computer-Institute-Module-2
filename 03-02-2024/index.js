2f0832690b0e8b7440faac9f087eba2c4a738a0d
Asadaziz <asadazizjs@gmail.com>
2024-03-09 16:56:01 +0500

    03-02-2024

---
+++ 03-02-2024/index.js
@@ -0,0 +1,23 @@
+const arr = [
+    {
+        name:"user",
+        email: "test1",
+        post: [
+            1,2,3
+        ]
+    },
+    {
+        name:"user",
+        email: "test1",
+        post: [
+            4,5,6
+        ]
+    },
+    {
+        name:"user",
+        email: "test1",
+        post: [
+            7,8,9
+        ]
+    }
+]
