39e419d25d94ac6f3ebb1802c359a61a72d476e0
Asadaziz <asadazizjs@gmail.com>
2024-03-23 15:11:19 +0500

    Started JS with variables( String and Number)

---
+++ Javascript/Class 1 (Intro)/script.js
@@ -0,0 +1,52 @@
+
+// chapter 1
+// alert("Hello world!    ");
+// window.alert('Window Alert'    );
+// alert('Hello world!    with single quotes');
+// Alert("Test Alert ");
+
+// chapter 2
+// camel case notation
+// var studentName;  // initialize
+// studentName = 'Zeeshan'; // assingment
+// var studentName1; // initialize // value = undefined 
+// var studentName2 = null;
+// // var 1212 = "xyz"; // not acceptable 
+// var _2312 = 'xyz'; // acceptable
+// var _$2312 = 'xyz'; // acceptable
+// var abc1212 = '23123'; // acceptable
+
+// alert (studentName);
+// alert(studentName1);
+// alert(studentName2);/ 
+// studentName1 = "Muzammil";
+// alert(studentName1);
+
+//chapter 3
+// var actualWeight = 160;
+// var age = 25;
+
+// var weightIncreased  = 10;
+// var increasedAge = 3;
+
+// alert(`Weight at the age of ${age} was ${actualWeight}`); // string interpolation
+// //alert('Weight at the age of ' + age + ' was ' + actualWeight); //string concatenation
+// alert(`Weight at the age of ${age + increasedAge} was ${actualWeight + weightIncreased }`);
+
+var age = 25;
+age = age + 5 
+console.log(age + 10);
+
+// chapter 5
+// var alert = 34; illegal
+// console.log(alert);
+
+//var suffah computer = 'Baldia town'; illegal
+
+// var rose = 'red';
+
+// var Rose = 'black' variables  are case sensitive
+
+
+
+
