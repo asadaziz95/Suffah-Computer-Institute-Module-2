217cd2b8eab83259e0b55abcb02e1054b1948368
Asadaziz <asadazizjs@gmail.com>
2024-03-24 13:53:57 +0500

    Class 2 Opertors

---
+++ Javascript/Class 2 ( Operators )/script.js
@@ -0,0 +1,81 @@
+// chapter 5 Opertors
+// creating marksheet
+
+// var english = 95;
+// var urdu = 75;
+// var maths = 90;
+// var pst = 85;
+// var biology = 95;
+// var totalMarks = 500;
+// var obtMarks = english +urdu +maths +pst+biology;
+// var percentage =  obtMarks / totalMarks * 100;
+// var number1 = 13;
+// var number2 = 2;
+
+// console.log(`Total marks ${totalMarks}`);
+// console.log(`Obtain marks ${obtMarks}`)
+// console.log(`Your percentage is ${percentage}%`)
+
+
+// console.log(`Modulus of two number is ${number1 % number2}`);
+
+
+// chapter 6 Unfamiliar operators
+
+// var num = 78;
+// num++; post icrement
+// num++;
+// num++;
+
+// num = num + 10;
+
+// num--;
+
+// var num1 = 1;
+// ++num1; pre icrement
+
+// console.log(num)
+
+// console.log(num1);
+
+
+
+// chapter 7 
+
+// var totalCost = 2 + 2 * 8;
+// console.log(totalCost) // 24 , 32 , 18
+
+// totalCost = 1 + (3*4);
+
+// console.log(totalCost)
+
+
+// totalCost =  (2 * 4) * 4 + 2;
+
+// console.log(totalCost)
+
+
+// totalCost = ((2 * 4) * 4) + 2;
+// console.log(totalCost);
+
+// totalCost = (2 * 4) * (4 + 2);
+
+// console.log(totalCost);
+
+// totalCost = 12 % 12 * 5 -96 +32;
+
+// console.log(totalCost)
+
+// chapter 8 Concatenating text strings
+
+var firstName = 'Ali';
+var lastName = 'Ahmed';
+
+console.log(firstName +" " + lastName);
+
+console.log('2' + 10);
+
+
+
+
+
