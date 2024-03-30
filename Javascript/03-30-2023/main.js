cf988348f174d688dbb6b71c1004e5848f8a5062
Asadaziz <asadazizjs@gmail.com>
2024-03-30 15:16:27 +0500

    Added js book

---
+++ Javascript/03-30-2023/main.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('button1');
+console.log("button" , button);
+// const colors = ['red','orange','yellow','green','blue','rebeccapurple','violet']
+
+// function change() {
+// document.body.style.background = colors[Math.floor(7*Math.random())];
+//   // console.log(button)
+//   // button.style.color =  colors[Math.floor(7*Math.random())];
+// }
+
+// button.addEventListener('click', change);
