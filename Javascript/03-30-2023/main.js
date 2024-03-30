967f75eecfeabc4e7ff0fb46bdd24303b8b3dae9
Asadaziz <asadazizjs@gmail.com>
2024-03-30 15:24:11 +0500

    Added js code for rainbow program

--- Javascript/03-30-2023/main.js
+++ Javascript/03-30-2023/main.js
@@ -1,11 +1,9 @@
 const button = document.getElementById('button1');
 console.log("button" , button);
-// const colors = ['red','orange','yellow','green','blue','rebeccapurple','violet']
+const colors = ['red','orange','yellow','green','blue','rebeccapurple','violet']
 
-// function change() {
-// document.body.style.background = colors[Math.floor(7*Math.random())];
-//   // console.log(button)
-//   // button.style.color =  colors[Math.floor(7*Math.random())];
-// }
+function change() {
+document.body.style.background = colors[Math.floor(7*Math.random())];
+}
 
-// button.addEventListener('click', change);
+button.addEventListener('click', change);
