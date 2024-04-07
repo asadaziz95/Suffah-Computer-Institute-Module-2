39f67ad05f460cfce6010f7ec2d2baaaafcf9eac
Asadaziz <asadazizjs@gmail.com>
2024-04-07 15:24:16 +0500

    Class 5 JS

---
+++ Javascript/Class 6/main.js
@@ -0,0 +1,75 @@
+// // take input from user and greet him with his nam using alert
+
+// // const Name= prompt  ("What is your name");
+// // console.log(Name)
+// // alert (Name)
+
+// // find if this sentense start with 'T' and ends with 'g'
+
+// //find the length of the string and display it in alert
+
+// const testString =  'The quick brown fox jumps over the lazy dog';
+
+// // console.log(testString.startsWith('T'));
+// // console.log(testString.endsWith('g'));
+
+// alert(testString.length)
+
+
+// const lastName = 'Ali';
+
+// const firstName = 'Ahmed'
+
+// // console.log(firstName +" "+lastName)
+
+// // alert(firstName +" "+lastName)
+
+// alert(firstName.concat(" "+lastName))
+
+// const userName = prompt('What is your full name?')
+// const lastCharMessage = 'Last character is ';
+// console.log(userName);
+
+
+// console.log(userName.length);
+
+// alert (' Your name is ' + userName);
+// alert('First character is ' + userName[0]);
+
+// alert( lastCharMessage + userName[userName.length-1]);
+
+
+// const testString = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available";
+
+// alert(testString.toLowerCase());
+// alert(testString.toUpperCase());
+
+
+// const textWithWhiteSpace = "    In publishing and graphic design, Lorem ipsum   ";
+// // console.log(textWithWhiteSpace);
+
+// // console.log(textWithWhiteSpace.trim());
+
+
+// console.log(`This is the ${textWithWhiteSpace.trim()}`)
+
+const animal = prompt('Please enter an animal.');
+const color = prompt('Please enter a color.');
+
+const verb = prompt('Please enter a verb.');
+const job = prompt('Please enter a job.');
+
+const madlib = `The ${animal} wanted to be a ${job}, but turned a funny shade of ${color} after trying to ${verb}!`
+
+
+console.log(madlib);
+
+
+
+
+
+
+
+
+
+
