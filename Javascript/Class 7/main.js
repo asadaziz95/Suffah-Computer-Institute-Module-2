647aa05894d16db62fe69231b06dc5b4a35ffe12
Asadaziz <asadazizjs@gmail.com>
2024-04-21 18:56:20 +0500

    Class 7

---
+++ Javascript/Class 7/main.js
@@ -0,0 +1,66 @@
+// let stu_Name = 'Ali'
+// let result = stu_Name*5;
+
+// console.log('type of' , typeof result);
+
+// console.log(result);
+
+
+
+// 2 ki power 4;
+
+// 2 * 2(4) * 2(8) * 2(16)
+// console.log(2**3);
+// 3 + 6/3
+// 3 + 2
+// 5
+// (8-5) + 6/3;
+
+//console.log(23%9);
+// console.log(2292%4)
+// console.log(9247%2);
+
+// let score = 15;
+
+// console.log(score++);
+// console.log(score);
+
+// console.log(++score);
+// console.log(score);
+
+//score = score + 1 // same as below
+
+//score +=1 short hand
+//score -=4
+
+// const ageInYears = Number(prompt('How old are you (in years)?'));
+// const ageInDays = ageInYears * 365.25;
+// const ageInHours = ageInYears * 365.25 * 24 * 60;
+// const ageInSeconds = ageInYears * 365.25 * 24 * 60 * 60;
+
+// alert(`That means you have been alive for at least ${ageInDays} days ${ageInHours} hours ${ageInSeconds} seconds!!`);
+
+// display days , hours , seconds
+
+
+
+// let randomNumber = 5 * Math.random()
+// alert('random Number generated' + " " +randomNumber)
+// alert('random Number without decimal' + " " + Math.ceil(randomNumber))
+// alert('random Number with floor' + " " + Math.floor(randomNumber))
+
+
+
+// const sides = prompt('How many sides does the dice have?');
+// alert('Press Enter or click close to roll the dice...');
+// const number = Math.ceil(sides*Math.random());
+// alert(`The dice landed on the number ${number}`);
+
+
+
+
+
+const sides = 6;
+alert('Press Enter or click close to roll the dice...');
+const number = Math.ceil(sides*Math.random());
+alert(`The dice landed on the number ${number}`);
