0125e009ef58cb0b3ebe4dc714d73f06f7a4565d
Asadaziz <asadazizjs@gmail.com>
2024-04-27 18:36:45 +0500

    Added class 8

---
+++ Javascript/Class 8/main.js
@@ -0,0 +1,52 @@
+/*
+Apple
+Banana
+Cupcake
+*/
+const emptyArray = [];
+const listOfItems = ['Apple','Banana','Cupcake']
+const listOfNumbers = ["4234435dfsdf",234234,23564645,74576,456456,456,456456,456,"fdsfs","sdfsdfsd"]
+const listOfBooleans = [true,false,false,true]
+console.log(listOfItems)
+// listOfItems[5] = 'Soda' 
+// listOfItems.length = 2;
+// console.log(listOfItems[3]);
+
+//listOfItems[3] = 'Banana Cake'
+//console.log(listOfItems[2])
+// listOfItems[2] = 'Cake'
+// console.log("length of listOfItems" ,listOfItems.length);
+// console.log("total no on indexes in listOfItems" ,listOfItems.length-1);
+// console.log("last item of array" , listOfItems[listOfItems.length-1]);
+// delete listOfItems[5]
+
+// listOfItems.pop();
+// console.log(listOfItems);
+
+// listOfItems.pop("Soda");
+// listOfItems.shift()
+// listOfItems.unshift("Avacado")
+
+// const arrA = ["Apple","Mangoes"]
+// const arrB = ["Cheery","Banana","Orange",...arrA]
+
+// console.log(arrB);
+
+
+const fruit = ['Pineapple', 'Melon'];
+const savory = ['Burger', 'Fries'];
+const sweets = ['Cookie', 'Popcorn'];
+
+
+// const fooda = [...fruit, ...savory, ...sweets];
+// console.log(fooda);
+// // fruit.pop();
+// const food = [fruit, savory, sweets];
+// console.log(food);
+
+
+
+// console.log(emptyArray);
+// console.log(listOfItems);
+// console.log(listOfNumbers);
+// console.log(listOfBooleans);
