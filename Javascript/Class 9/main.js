120141983cc099e3c37f44e9736ac800b7a11e61
Asadaziz <asadazizjs@gmail.com>
2024-04-28 18:57:05 +0500

    Class 9 JS

--- Javascript/Class 9/main.js
+++ Javascript/Class 9/main.js
@@ -22,7 +22,7 @@ const studentsName = ["Hammad","Ahmed","Affan","Orhan","Ahmed","Afridi","Bahadur
 // const studentIndex = studentsName.indexOf("Ahmed");
 // const studentIndex = studentsName.indexOf("Ahme");
 // studentsName.splice(studentIndex,1,"Testing")
-console.log(studentsName);
+// console.log(studentsName);
 // console.log(studentIndex);
 
 //includes
@@ -34,9 +34,44 @@ console.log(studentsName);
 // const result = studentsName.reverse()
 // const result = studentsName.sort()
 // console.log(typeof result);
-console.log(result);
+//console.log(result);
 
 // console.log([52,2,3453,534534,4,51,6,7,8,9,8787,22323232,10,11,12].sort());
+
+//split
+// const namesOfStudents = "asad,ahmed,ali"
+// console.log(namesOfStudents.split(","));
+// console.log(namesOfStudents); 
+
+
+//multidimensional array
+// const multDimArr = [[1,3],[2,4]]
+
+// const summer = ['Jun', 'Jul', 'Aug'];
+// const winter = ['Dec', 'Jan', 'Feb'];
+// // const nested = [ ...summer, ...winter ];
+// const nested = [ summer, winter ].flat();
+// console.log(nested);
+//<< [ [ 'Jun', 'Jul', 'Aug' ], [ 'Dec', 'Jan', 'Feb' ] ]
+
+
+// const user =prompt('write a list of name');
+// const userSorted =user.split(",").sort().join(",");
+// alert(userSorted);
+
+
+
+
+// Use three prompt boxes to ask the user for three different words and
+// then place them in an array. Use an alert box to display the array. 
+
+// let word1 = prompt('type a name');
+// let word2 = prompt('type a name');
+// let word3 = prompt('type a name');
+
+// let wordArray = [word1, word2, word3]
+// alert(wordArray);
+
 
 
 
