7ea370d133a127fa60d692c7f5dd1e441646bc4b
Asadaziz <asadazizjs@gmail.com>
2024-04-28 17:24:04 +0500

    Class 9 JS

---
+++ Javascript/Class 9/main.js
@@ -0,0 +1,43 @@
+const arrA = ['ali','ahmed'];
+// const arrB = ['Affan'];
+
+// const arrC = [...arrA,...arrB]
+
+// console.log(arrC);
+
+//slicing
+
+// const food = ['Coconut', 'Pineapple', 'Melon', 'Burger', 'Fries', 'Cookie', 'Popcorn','Coffee']
+// const extractedFood = food.slice(0,5);
+// console.log(food);
+// console.log(extractedFood);
+// console.log('Original array',food);
+// const extractedArray = food.splice(2, 5,...arrA);
+// const extractedArray = food.splice(2, 5,'Mango');
+// console.log(food);
+// console.log(extractedArray);
+
+//indexOf
+const studentsName = ["Hammad","Ahmed","Affan","Orhan","Ahmed","Afridi","Bahadur"];
+// const studentIndex = studentsName.indexOf("Ahmed");
+// const studentIndex = studentsName.indexOf("Ahme");
+// studentsName.splice(studentIndex,1,"Testing")
+console.log(studentsName);
+// console.log(studentIndex);
+
+//includes
+
+// const result = studentsName.includes("Orhan");
+
+// const result = studentsName.includes("Orhan",4);
+// const result = studentsName.join(" * ")
+// const result = studentsName.reverse()
+// const result = studentsName.sort()
+// console.log(typeof result);
+console.log(result);
+
+// console.log([52,2,3453,534534,4,51,6,7,8,9,8787,22323232,10,11,12].sort());
+
+
+
+
