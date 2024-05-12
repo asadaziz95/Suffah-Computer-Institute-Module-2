5d4cd27393706388967e3fa6e08c9751a5bfbe50
Asadaziz <asadazizjs@gmail.com>
2024-05-12 19:11:55 +0500

    Class 13 (JS)

---
+++ Javascript/Class 13/main.js
@@ -0,0 +1,103 @@
+// // function hello(){
+// //   for(var i=1;i<=10;i++){
+// //      console.log("Hello World"+i);
+// //   }
+ 
+// // }
+// // //second page
+// // hello();
+// //annonymus function
+
+// // const goodbye = function(){
+// // alert('Goodbye, World!');
+// // };
+
+// // goodbye();
+
+
+// // const hola1 = function() {alert('Hola, Mundo!')};
+
+// // const hola = () => alert('Hola, Mundo arrow!');
+
+// // hola1()
+// // hola()
+
+
+
+// // const calculator = () =>{
+// //   var result = 1+9;
+// //   return result;
+// // }
+// // const result = calculator();
+// // console.log(result);
+
+// const getMonthName =(monthNumber)=>{
+//   switch(monthNumber){
+//     case 1:
+//       return 'January'
+//         case 2:
+//       return 'Febrarury'
+//           case 3:
+//       return 'March'
+//           case 4:
+//       return 'April'
+//           case 5:
+//       return 'May'
+//           case 6:
+//       return 'June'
+//           case 7:
+//       return 'July'
+//           case 8:
+//       return 'August'
+//           case 9:
+//       return 'Szptmeber'
+//           case 10:
+//       return 'October'
+//           case 11:
+//       return 'November'
+//           case 12:
+//       return 'December'
+//       default :
+//       return "Not a valid month number"
+// }
+// }
+
+// const monthNumber = getMonthName(13)
+// //console.log(monthNumber)
+
+// const square = n => n*n;
+
+// const userChoice =Number(prompt("Enter you choice"));
+
+// for(var i = 1; i<=userChoice;i++){
+//   console.log(square(i));
+// }
+
+
+//  function square(n){
+//     return n*n;
+//     }
+
+//console.log(square(5));
+
+// const mean = (a,b,c,d=0) =>(a+b+c+d)/4
+
+// console.log(mean(2,54,76))
+
+// function hello (name='World'){
+//   alert(`Hello, ${name}!`);
+// }
+
+// hello("Hassan Afridi")
+// hello()
+
+
+
+
+
+
+
+
+
+
+
