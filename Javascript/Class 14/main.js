141db42c5a670dc4e7325227cd8c3db69ecc1ad2
Asadaziz <asadazizjs@gmail.com>
2024-05-18 19:14:43 +0500

    Class 14 JS

---
+++ Javascript/Class 14/main.js
@@ -0,0 +1,75 @@
+// const randomInt = (n,userName = 'ABC')=>{
+//     return `${userName} ${Math.ceil(Math.random()*n)}`;
+// }
+// console.log(randomInt(5,"Ali"));
+// console.log(randomInt(8,"Ahmed"));
+
+// function randomInt(lower, upper) {
+//   if (upper === undefined) {
+//     upper = lower;
+//     lower = 1;
+//   }
+//   console.log("upper",upper);
+//   console.log("lower",lower);
+//   return Math.floor(Math.random() * (upper - lower + 1) + lower);
+// }
+
+// console.log(randomInt(3));
+
+// const messageGenertor=(userName)=>{
+// console.log(`Welcome ${userName}`)
+// }
+// const result = messageGenertor('Asad');
+// alert(result)
+
+
+// const userIsChild = function(age){
+//     if(age<18){
+//     return true;
+//     } else {
+//     return false;
+//     }
+//     }
+// console.log(userIsChild(17));
+
+// const ourFirstObj = {};
+// const square = {
+//     sides:4,
+//     length: 5,
+// }
+// const batch2Class = {
+//     students:25,
+//     teacher:1,
+//     workstation:40,
+//     led:3,
+//     fan:6,
+//     learning:function(){
+//         console.log('Average')
+//     },
+//     teaching:function(){
+//         console.log('Average')
+//     },
+//     interest:"40%"
+// }
+
+// mini guess game
+// const alfie = {
+//     name: 'Alfie',
+//     picture: '&#128512',
+//     glasses: no,
+//     hat: no
+//     }
+
+const nameEl = 'Dumbo';
+const legs = 4;
+const fly = () => console.log('Fly, fly away!');
+
+const elephant = { nameEl, legs, fly,color:"Grey" };
+const elephant1 = {
+    nameEl: nameEl,
+    legs: legs,
+    fly: fly
+    }
+
+
+    
